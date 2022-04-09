315aa6eaffe7bea5b4c67350e36bebc6ad700ecd
hamza-python <hamzabhatti860@gmail.com>
2022-04-09 15:15:51 +0500

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {    //use stf
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(     //using container to set background image
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/login.webp'), fit: BoxFit.cover)
+      ),
+    child:Scaffold(  //for normal scaffold use
+      backgroundColor: Colors.transparent, //to show backgound image bcz default is WHITE in scaffold property
+    ),
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:project/login.dart';
+
+void main() {
+  runApp(MaterialApp(   //widget with more features i.e nav, themes
+    debugShowCheckedModeBanner: false, // remove debug banner from screen
+    initialRoute: 'login', // 1st goes to login page
+    routes: {
+      'login': (context) => MyLogin() //class name in which login page is set
+    },
+  ));
+}
+
+
